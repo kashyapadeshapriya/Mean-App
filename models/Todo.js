f5bd7d04ae0356c9f254c891342bd512f514a66b
kashyapadeshapriya <kashyapa.deshapriya@gmail.com>
2015-12-03 00:02:52 +0530

    testing data 12/02

--- models/Todo.js
+++ models/Todo.js
@@ -2,15 +2,10 @@ var mongoose = require('mongoose');
 
 
 var TodoSchema = new mongoose.Schema({
-
-  
 		name: String,
- 
- 		age : number, 
- 		note: String
-
-
+ 		age : Number, 
+ 		note: String,
+		reg_no : Number
 });
-
 
 module.exports = mongoose.model('Todo', TodoSchema);
